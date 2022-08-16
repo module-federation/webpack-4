025f2dfee2f9b65d1c7b8668b962e2019bd3e9c4
张洪恩 <hongen.zhang@weimob.com>
2022-08-16 11:32:27 +0800

    减少依赖

--- examples/hot-refresh/app1/babel.config.js
+++ examples/hot-refresh/app1/babel.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", {
+    ["@babel/preset-react", {
       targets: {
         browsers: ["> 1%", "last 2 versions", "not ie <= 8"],
       },
     }],
   ],
   plugins: [
-    "@babel/plugin-transform-runtime",
-    "transform-react-jsx",
     "react-refresh/babel",
   ],
   sourceType: "unambiguous"
